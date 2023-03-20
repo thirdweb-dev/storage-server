cc83dc9b9720d86bd0e69f4ac09ea3c69ca22875
Danny Friday <nessup@gmail.com>
2023-03-20 15:48:32 -0700

    Fix environment variables

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -5,7 +5,7 @@ const dataSource = new DataSource({
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USERNAME,
+  username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_NAME,
   synchronize: false,
