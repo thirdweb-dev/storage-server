0ea3726a6e4e8e1cda479af9db3a956f5302eb00
Danny Friday <nessup@gmail.com>
2023-04-06 20:04:52 -0700

    Finish POC with streaming directory upload

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import express from 'express';
 import dataSource from './ormconfig';
 import { Client } from '@web3-storage/w3up-client';
+import { createWriter } from '@ipld/unixfs'
 import busboy from 'busboy'
 import nodeStream from 'node:stream';
 import events from 'node:events';
@@ -13,6 +14,8 @@ import { importDAG } from '@ucanto/core/delegation'
 import { Block } from '@ipld/car/reader';
 import PQueue from 'p-queue';
 import cors from 'cors'
+import * as CAR from '@web3-storage/upload-client/car'
+import { AnyLink, DirectoryEntryLink } from '@web3-storage/upload-client/dist/src/types';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -26,16 +29,20 @@ app.use(cors())
 // app.use(bodyParser.json());
 
 app.post('/uploads',  async (req, res) => {
-  const bb = busboy({ headers: req.headers })
+  // Allow a long time for uploads
+  req.setTimeout(360000000)
+
+  const bb = busboy({ headers: req.headers, preservePath: true })
   const workQueue = new PQueue({ concurrency: 1 });
 
-  function abort() {
+  function abort(e: Error) {
     console.log('aborted')
     req.unpipe(bb);
     workQueue.pause();
     if (!req.aborted) {
       res.set("Connection", "close");
-      res.sendStatus(413);
+      // TODO: Surface user errors as Bad Request, all other errors as Internal Server Error
+      res.status(500).send(e.message);
     }
   }
 
@@ -43,40 +50,99 @@ app.post('/uploads',  async (req, res) => {
     workQueue.add(async () => {
       try {
         await fn();
-      } catch (e) {
-        abort();
+      } catch (e: any) {
+        console.error(e)
+        abort(e);
       }
     });
   }
 
-  bb.on('file', async (name, file, info) => {
-    console.log('file incoming');
+  // Used to track individual file entry points for a directory upload
+  const directoryEntries: DirectoryEntryLink[] = []
+
+  // Track when the form is done parsing
+  let formDone = false;
+
+  // Track when a directory upload has begun
+  let directoryUploadBegan = false;
+
+  bb.on('file', async (_, file, info) => {
     abortOnError(async () => {
-      const { filename, encoding, mimeType } = info;
+      const { filename: filePath, encoding, mimeType } = info;
       console.log(
-        `File [${name}]: filename: %j, encoding: %j, mimeType: %j`,
-        filename,
+        `File: filePath: %j, encoding: %j, mimeType: %j`,
+        filePath,
         encoding,
-        mimeType
+        mimeType,
+        info
       );
 
-      req.setTimeout(360000000)
-
       file
-        .on('data', (data) => {
-          console.log(`File [${name}] got ${data.length} bytes`);
-          // fileData.push(data)
-        })
-        .on('close', () => {
-          console.log(`File [${name}] done`);
+        .on('close', async () => {
+          console.log(`File [${filePath}] done`);
         });
 
-      console.log('started uploading')
-      console.time('started uploading')
-      const cid = await client.uploadFile({
-        stream: () => nodeStream.Readable.toWeb(file) as any,
-      });
-      console.timeEnd('started uploading')
+      if (filePath !== 'file' && !filePath.startsWith('files/')) {
+        throw new Error(`Invalid file name: ${name}. Must be "file" for a single file upload, or "files/{name}" for a directory upload (note the 's' in "files")`)
+      }
+
+      const isPartOfDirectory = filePath.startsWith('files/');
+      if (isPartOfDirectory) {
+        // If the filename variable doesn't include a filename (e.g., it's just "files/"), we need to throw an error letting the user know that they need to include a filename
+        if (filePath === 'files/') {
+          throw new Error(`Invalid file name: it cannot be blank. When uploading a file part of a directory, you must include a filename for each file (e.g. "files/my-file.txt")`)
+        }
+
+        // // If the filePath includes multiple slashes (e.g., "files/my-folder/my-file.txt"), we need to throw an error letting the user know that they can only upload one file at a time
+        if (filePath.split('/').length > 2) {
+          throw new Error(`Invalid file name: ${filePath}. When uploading a file part of a directory, you can not include subdirectories. We will add support for subdirectories very soon.`)
+        }
+
+        // Remove the "files/" prefix to get the file's actual name
+        const fileName = filePath.slice('files/'.length);
+
+        // Upload the individual file needed for the directory upload
+        // TODO: Work with the w3s team to eliminate the need for a directory upload. This should be a single file upload
+        console.log('Starting upload of file in directory...')
+        console.time('Upload took')
+        const entries: DirectoryEntryLink[] = []
+        await client.uploadDirectory([
+          {
+            name: fileName,
+            stream: () => nodeStream.Readable.toWeb(file) as any,
+          }
+        ], { onDirectoryEntryLink: e => entries.push(e) })
+        console.timeEnd('Upload took')
+        // console.log('got entries', entries)
+
+        // Add the entry to the directoryEntries array
+        directoryEntries.push(entries[0])
+
+        // If the form is done parsing, we can upload the directory
+        if (formDone && directoryEntries.length && !directoryUploadBegan) {
+          console.log('Starting dir upload from file body')
+          directoryUploadBegan = true
+          const cid = await uploadDir(client, directoryEntries)
+          console.log('Directory CID =', cid.toString())
+          res.json({
+            IpfsHash: cid.toString(),
+          })
+          console.log('responded')
+        }
+      } else {
+        console.log('Starting upload of individual file...')
+        console.time('Upload took')
+        const cid = await client.uploadFile({
+          stream: () => nodeStream.Readable.toWeb(file) as any,
+        });
+        console.timeEnd('Upload took')
+
+        res.json({
+          IpfsHash: cid.toString(),
+        })
+
+        console.log('CID =', cid.toString())
+      }
 
       // // Track the upload
       // const upload = new UploadEntity();
@@ -85,16 +151,25 @@ app.post('/uploads',  async (req, res) => {
       //
       // res.json(upload);
 
-      res.json({
-        IpfsHash: cid
-      })
     })
-
     bb.on('field', (name, val, info) => {
       console.log(`Field [${name}]: value: %j`, val);
     });
-    bb.on('close', () => {
+    bb.on('close', async () => {
       console.log('Done parsing form!');
+
+      formDone = true;
+
+      // If the form is done parsing, we can upload the directory
+      // if (formDone && directoryEntries.length && !directoryUploadBegan) {
+      //   console.log('Starting dir upload from handler for done parsing form')
+      //   directoryUploadBegan = true
+      //   const cid = await uploadDir(client, directoryEntries)
+      //   res.json({
+      //     IpfsHash: cid.toString(),
+      //   })
+      // }
+
       // res.json({ ok: true })
       // res.writeHead(303, { Connection: 'close', Location: '/' });
       // res.end();
@@ -132,4 +207,32 @@ async function parseProof (data: string) {
     blocks.push(block)
   }
   return importDAG(blocks as any)
+}
+
+async function uploadDir (client: Client, entries: DirectoryEntryLink[]): Promise<AnyLink> {
+  console.log('Creating directory CAR...');
+  const { readable, writable } = new TransformStream({})
+  const unixfsWriter = createWriter({ writable })
+  const dirWriter = unixfsWriter.createDirectoryWriter()
+  for (const entry of entries) {
+    // @ts-expect-error
+    dirWriter.set(entry.name, entry)
+  }
+  console.log('1')
+  await dirWriter.close()
+  console.log('2')
+  unixfsWriter.close()
+
+  const blocks: any[] = []
+  await readable.pipeTo(new WritableStream({ write: b => { blocks.push(b) } }))
+  console.log('3')
+  const car = await CAR.encode(blocks)
+
+  console.log('Uploading directory CAR...')
+  console.time('Directory CAR upload took')
+  const cid = await client.uploadCAR(car)
+  console.log('4')
+  console.timeEnd('Directory CAR upload took')
+
+  return cid
 }
